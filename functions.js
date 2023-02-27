139dd1efae97ed1146c1e69e8083c897f5a610c7
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-27 14:34:40 -0700

    learning JS unit testing

--- functions.js
+++ functions.js
@@ -1,9 +1,25 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function sayHello(name) {
+  if (name === 'Will') {
+    return `No more testing ${name}!`;
+  } else {
+    return `Hi there ${name}!`;
+  }
+}
+
+function buildCar(color, type) {
+  let car = {};
+  if (color) car.color = color;
+  if (type) car.type = type;
+  return car;
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
